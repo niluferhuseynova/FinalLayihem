098aa119978c2da0a77fbbd06ca5b239d01a68e4
niluferhuseynova <niluferhuseynova144@gmail.com>
2026-02-18 21:34:02 +0400

    initial

---
+++ Feane/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Feane.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
